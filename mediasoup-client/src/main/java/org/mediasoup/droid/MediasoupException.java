20442f667d3b0a4a1b92b65be58f2cee90460ef1
dongxl <1063273302@qq.com>
2020-06-12 12:52:26 +0800

    MediasoupException 存在bug

--- mediasoup-client/src/main/java/org/mediasoup/droid/MediasoupException.java
+++ mediasoup-client/src/main/java/org/mediasoup/droid/MediasoupException.java
@@ -1,3 +1,27 @@
 package org.mediasoup.droid;
 
-public class MediasoupException extends Exception {}
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+public class MediasoupException extends Exception {
+    public MediasoupException() {
+        super();
+    }
+
+    public MediasoupException(String message) {
+        super(message);
+    }
+
+    public MediasoupException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MediasoupException(Throwable cause) {
+        super(cause);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    protected MediasoupException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
